cbdd1d311c1a067c99af41d9e88867bd3bc1a18d
Marshall Clow <marshall@idio.com>
2016-01-27 22:27:10 -0800

    Better sed style

--- getFSList.sh
+++ getFSList.sh
@@ -4,7 +4,7 @@ TODAY=`date "+%Y%m%d"`
 
 curl "https://familysearch.org/search/collectioninfo?summary=true&facets=OFF&offset=00&count=3000" | sed -e 's/{"collectionId":/\
   &/g' -e 's/],/\
-],/g' > "FSList_$TODAY.json"
+&/g' > "FSList_$TODAY.json"
 
 # curl "https://familysearch.org/search/collectioninfo?summary=true&facets=OFF&offset=00&count=3000" -o "FSList_$TODAY.json"
 
