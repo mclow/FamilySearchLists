3c60f4e046fa62759ae96492adca7e183604fa35
Marshall Clow <marshall@idio.com>
2016-10-15 09:25:20 -0700

    Enhace reporting; report changes to collection as well as the fact that they've changed

--- compareJSON.py
+++ compareJSON.py
@@ -31,65 +31,115 @@ def toDict(collection):
 		d [ item [u'collectionId']] = item
 	return d
 
-def printCollection(coll):
-	imgCnt = int(coll[u'count'])
-	if imgCnt > 0:
-		imgStr = "%s indexed records with record images" % locale.format("%d", imgCnt, grouping=True)
+def imageString(idxCount, imgCount):
+	idxStr = locale.format("%d", idxCount, grouping=True)
+	imgStr = locale.format("%d", imgCount, grouping=True)
+	if idxCount > 0 and imgCount > 0:
+		retStr = "%s indexed records with %s record images" % (idxStr, imgStr)
+	elif imgCount > 0:
+		retStr = "Browse %s Images only, no index" % (imgStr)
+	elif idxCount > 0:
+		retStr = "Index only (%s records), no images" % (idxStr)
 	else:
-		imgStr = "Browse Images only, no index"
-	print "%s\t(https://familysearch.org/search/collection/%s); %s, Updated %s" % (coll[u'title'], coll[u'collectionId'], imgStr, coll[u'lastUpdate'])
+		retStr = "Index only, no images"
+	return retStr
+
+def imageStringWithDiffs(idxCount, imgCount, oldCount, oldImages):
+	retStr = imageString(idxCount, imgCount)
+	oldidxStr = locale.format("%d", oldCount, grouping=True)
+	oldimgStr = locale.format("%d", oldImages, grouping=True)
+	retStr += " (was %s records with %s images)" % (oldidxStr, oldimgStr)
+	return retStr
+	
+def printCollection(coll, action):
+	idxCnt = int(coll[u'count'])
+	imgCnt = int(coll[u'count'])
+	imgStr = imageString(idxCnt, imgCnt)
+	print "%s\t(https://familysearch.org/search/collection/%s); %s, %s %s" % (coll[u'title'], coll[u'collectionId'], imgStr, action, coll[u'lastUpdate'])
+
+def printCollectionWithDiffs(newColl, oldColl, action):
+	idxCnt = int(newColl[u'count'])
+	imgCnt = int(newColl[u'imageCount'])
+	imgStr = imageStringWithDiffs(idxCnt, imgCnt, int(oldColl[u'count']), int(oldColl[u'imageCount']))
+	print "%s\t(https://familysearch.org/search/collection/%s); %s, %s %s" % (newColl[u'title'], newColl[u'collectionId'], imgStr, action, newColl[u'lastUpdate'])
 
 
-def printDict(d, label):
+def printDict(d, label, action):
 	print label
 	for k in d.keys():
-		printCollection(d[k])
+		printCollection(d[k], action)
 #		print "%s\t(https://familysearch.org/search/collection/%s); xxx, Updated %s" % (d[k][u'title'], d[k][u'collectionId'], d[k][u'lastUpdate'])
 
+def printDictWithDiffs(d, label, action):
+	print label
+	for k in d.keys():
+		printCollectionWithDiffs(newEntries[k], oldEntries[k], action)
+#		print "%s\t(https://familysearch.org/search/collection/%s); xxx, Updated %s" % (d[k][u'title'], d[k][u'collectionId'], d[k][u'lastUpdate'])
 
 # {"collections":[{"collectionId":"1974186","title":"Argentina, Jujuy, Catholic Church Recor
 locale.setlocale(locale.LC_ALL, 'en_US')
-j1 = readJSON(sys.argv[1])[u"collections"]
-j2 = readJSON(sys.argv[2])[u"collections"]
+oldJSON = readJSON(sys.argv[1])[u"collections"]
+newJSON = readJSON(sys.argv[2])[u"collections"]
 
 
 # Given the two collections, generate four new collections.
 # Unchanged, added, removed, updated
 
 # key on collection ID for faster lookup
-d1 = toDict(j1)
-d2 = toDict(j2)
+oldEntries = toDict(oldJSON)
+newEntries = toDict(newJSON)
 
 added = {}
 removed = {}
 updated = {}
 moreImages = {}
+fewerImages = {}
+moreRecords = {}
+fewerRecords = {}
 unchanged = {}
 
-# Find deleted, updated, and unchanged collections
-for coll in j1:
+# Find added, updated, and unchanged collections
+for coll in newJSON:
 	id = coll [u'collectionId']
-	if not d2.has_key(id):
-		removed[id] = coll
+	if not oldEntries.has_key(id):
+		added[id] = coll
 	else:
-		assert(d2[id][u'lastUpdateMillis'] >= d1[id][u'lastUpdateMillis'])
-		if d2[id][u'lastUpdateMillis'] > d1[id][u'lastUpdateMillis']:
+		oldColl = oldEntries[id]
+		newColl = newEntries[id]
+		assert(newColl[u'lastUpdateMillis'] >= oldColl[u'lastUpdateMillis'])
+		if   newColl[u'lastUpdateMillis'] > oldColl[u'lastUpdateMillis']:
 			updated[id] = coll
-		elif d2[id][u'imageCount'] > d1[id][u'imageCount']:
+		elif newColl[u'imageCount']       > oldColl[u'imageCount']:
 			moreImages[id] = coll
+		elif newColl[u'imageCount']       < oldColl[u'imageCount']:
+			fewerImages[id] = coll
+		elif newColl[u'count']       > oldColl[u'count']:
+			moreRecords[id] = coll
+		elif newColl[u'count']       < oldColl[u'count']:
+			fewerRecords[id] = coll
 		else:
-#			assert(d2[id][u'count'] == d1[id][u'count'])
+#			assert(newColl[u'count'] == oldEntries[id][u'count'])
 			unchanged[id] = coll
 
 # Find added collections
-for coll in j2:
+for coll in oldJSON:
 	id = coll [u'collectionId']
-	if not d1.has_key(id):
-		added[id] = coll
+	if not newEntries.has_key(id):
+		removed[id] = coll
 
-printDict(removed, "--- Collections Deleted ---")
-printDict(added,   "--- Collections Added   ---")
-printDict(updated, "--- Collections Updated ---")
-printDict(moreImages, "--- Collections with new images ---")
+printDict(removed, "--- Collections Deleted ---", "DELETED")
+print
+printDict(added,   "--- Collections Added   ---", "ADDED")
+print
+printDictWithDiffs(updated, "--- Collections Updated ---", "Updated")
+print
+printDictWithDiffs(moreImages, "--- Collections with new images ---", "")
+print
+printDictWithDiffs(fewerImages, "--- Collections with images removed ---", "")
+print
+printDictWithDiffs(moreRecords, "--- Collections with new records ---", "")
+print
+printDictWithDiffs(fewerRecords, "--- Collections with records removed ---", "")
+print
 
-# print len(j1), len (j2), len(unchanged)
+# print len(oldJSON), len (newJSON), len(unchanged)
