468752fca8afc4327aecf66a90125f5bd341c25c
Marshall Clow <marshall@idio.com>
2017-09-08 20:05:40 -0700

    Run curl in silent mode

--- getAncestryList.sh
+++ getAncestryList.sh
@@ -6,7 +6,7 @@ echo $YOUAREHERE
 TODAY=`date "+%Y%m%d"`
 
 # get the data from familysearch.org; break it into lines, and save it into a file
-curl "http://www.ancestry.com/cs/recent-collections" | grep "data-jsondata" | "$YOUAREHERE/format_ancestry.sh" > "AncestryList_$TODAY.json"
+curl -sS "http://www.ancestry.com/cs/recent-collections" | grep "data-jsondata" | "$YOUAREHERE/format_ancestry.sh" > "AncestryList_$TODAY.json"
 
 # Compare with what we started with
 # ./compareJSON.py AncestryList_base.json "AncestryList_$TODAY.json" 
--- getFSList.sh
+++ getFSList.sh
@@ -3,7 +3,7 @@
 YOUAREHERE=$( dirname "${BASH_SOURCE[0]}" )
 
 # get the data from familysearch.org; break it into lines, and save it into a file
-curl "https://www.familysearch.org/search/collectioninfo?summary=true&facets=OFF&offset=00&count=3000" | "$YOUAREHERE/format_collection.sh" > "today.json"
+curl -sS "https://www.familysearch.org/search/collectioninfo?summary=true&facets=OFF&offset=00&count=3000" | "$YOUAREHERE/format_collection.sh" > "today.json"
 
 # Compare with what we started with
 ./compareJSON.py FSList_base.json "today.json" 
